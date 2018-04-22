3413b3f9431864c21005079ee8237701851a17d9
Marcos Monteiro <marcosmmb90@gmail.com>
2018-04-22 13:10:38 -0300

    more organized code

--- connection.py
+++ connection.py
@@ -1,54 +1,67 @@
-
 import struct
 import time
 import random
 from hashlib import sha256
 import socket
 
-HOST = "177.190.148.9"
-PORT = 8333
-BLOCK_HEIGHT = 519202
-PROTOCOL_VERSION = 70015
+class Connection:
 
-def return_payload(block_height, protocol_version):
-    version = struct.pack("i", protocol_version)
-    services = struct.pack("Q", 0)
-    timestamp = struct.pack("q", time.time())
-    addr_recv_services = struct.pack("Q", 0)
-    addr_recv_ip = struct.pack(">16s", "127.0.0.1")
-    addr_recv_port = struct.pack(">H", 8333)
-    addr_trans_services = struct.pack("Q", 0)
-    addr_trans_ip = struct.pack(">16s", "127.0.0.1")
-    addr_trans_port = struct.pack(">H", 8333)
-    nonce = struct.pack("Q", random.getrandbits(64))
-    user_agent_bytes = struct.pack("B", 0)
-    start_height = struct.pack("i", block_height)
-    relay = struct.pack("?", False)
+    def __init__(self,  
+                host = "177.190.148.9", 
+                port = 8333, 
+                block_height = 519202, 
+                protocol_version = 70015): # https://bitcoin.org/en/developer-reference#protocol-versions
 
-    payload = version + services + timestamp + addr_recv_services + \
-    addr_recv_ip + addr_recv_port + addr_trans_services + addr_trans_ip + \
-    addr_trans_port + nonce + user_agent_bytes + start_height + relay
-    
-    return payload
+        self.host = host
+        self.port = port
+        self.block_height = block_height
+        self.protocol_version = protocol_version
 
-def return_msg():
-    payload = return_payload(BLOCK_HEIGHT, PROTOCOL_VERSION)
-    magic = "F9BEB4D9".decode("hex")
-    command = "version" + 5 * "\00"
-    length = struct.pack("I" ,len(payload))
-    checksum = sha256(sha256(payload).digest()).digest()[:4]
+    def return_payload(self):
+        # https://bitcoin.org/en/developer-reference#version
+        version = struct.pack("i", self.protocol_version)
+        services = struct.pack("Q", 0)
+        timestamp = struct.pack("q", time.time())
+        addr_recv_services = struct.pack("Q", 0)
+        addr_recv_ip = struct.pack(">16s", "127.0.0.1")
+        addr_recv_port = struct.pack(">H", 8333)
+        addr_trans_services = struct.pack("Q", 0)
+        addr_trans_ip = struct.pack(">16s", "127.0.0.1")
+        addr_trans_port = struct.pack(">H", 8333)
+        nonce = struct.pack("Q", random.getrandbits(64))
+        user_agent_bytes = struct.pack("B", 0)
+        start_height = struct.pack("i", self.block_height)
+        relay = struct.pack("?", False)
 
-    msg = magic + command + length + checksum + payload
-    
-    return msg
+        payload = version + services + timestamp + addr_recv_services + \
+        addr_recv_ip + addr_recv_port + addr_trans_services + addr_trans_ip + \
+        addr_trans_port + nonce + user_agent_bytes + start_height + relay
+        
+        return payload
 
-msg = return_msg()
+    def return_msg(self):
+        # https://bitcoin.org/en/developer-reference#message-headers
+        payload = self.return_payload()
+        magic = "F9BEB4D9".decode("hex")
+        command = "version" + 5 * "\00"
+        length = struct.pack("I" ,len(payload))
+        checksum = sha256(sha256(payload).digest()).digest()[:4]
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        msg = magic + command + length + checksum + payload
+        
+        return msg
 
-s.connect((HOST, PORT))
+    def start_connection(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.port))
+        return s
 
-s.send(msg)
+    def send_msg(self):
+        msg = self.return_msg()
+        s = self.start_connection()
+        s.send(msg)
+        r = s.recv(1024)
+        return r
 
-s.recv(1024)
-
+conn = Connection()
+conn.send_msg()
